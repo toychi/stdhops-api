6bbbd8cdf588af1020b3c4a5a783f53ba22bfdb6
Chitipat Thabsuwan <chitipat.toy@gmail.com>
2019-02-13 23:00:53 +0700

    - add api that return results of clustering - modify Dockerfile - add bkk json file

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM python:3.6-alpine
-RUN adduser -D stdhops
+FROM python:3
+RUN adduser --disabled-password --gecos '' stdhops
 ADD . /home/stdhops/app
 WORKDIR /home/stdhops/app
 EXPOSE 4000
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+RUN pip3 install --upgrade pip
+RUN pip3 install --upgrade setuptools
+RUN pip3 install numpy pandas scikit-learn geopandas
+RUN pip3 install -r requirements.txt
 ENTRYPOINT ["python", "index.py"]
--- app/controllers/houses.py
+++ app/controllers/houses.py
@@ -2,7 +2,13 @@
 import os
 from flask import request, jsonify, make_response
 from app import app, mongo
+import pandas as pd
+from pandas.io.json import json_normalize
+import re
+from sklearn import cluster
+import geopandas as gpd
 
+bkk_link = 'app/controllers/bkk_geo.json'
 
 @app.route('/house')
 def house():
@@ -10,3 +16,32 @@ def house():
     resp = make_response(jsonify(data), 200)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
+@app.route('/mapcluster')
+def mapcluster():
+    data = mongo.db.house.find()
+    k = json_normalize(list(data))
+    k[['bedroom','bathroom','parking']] = pd.DataFrame(k.room.values.tolist())
+    k[['lat','lng']] = pd.DataFrame(k.latlng.values.tolist())
+    k['bedroom'] = k['bedroom'].str.replace('\D','')
+    k['bathroom'] = k['bathroom'].str.replace('\D','')
+    k['parking'] = k['parking'].str.replace('\D','')
+    k['price'] = k['price'].str.replace('\D','')
+    k.loc[k.area.str.contains('ตารางวา', na=False),'area'] = pd.to_numeric(k.loc[k.area.str.contains('ตารางวา', na=False)]['area'].str.replace('\D','')) * 4
+    k.loc[k.area.str.contains('ตารางเมตร', na=False),'area'] = pd.to_numeric(k.loc[k.area.str.contains('ตารางเมตร', na=False)]['area'].str.replace('\D',''))
+    k.loc[k.area.str.contains('ไร่', na=False),'area'] = pd.to_numeric(k.loc[k.area.str.contains('ไร่', na=False)]['area'].str.replace('\D','')) * 1600
+    k = k.fillna(0)
+    km5 = cluster.KMeans(n_clusters=5)
+    k = k.drop(['_id','date','id','latlng','name','room'], axis=1)
+    for tag in ('price', 'area', 'bedroom', 'bathroom', 'parking'):
+        k[tag] = pd.to_numeric(k[tag])
+    g_k = k.groupby('district').mean()
+    bkk = gpd.read_file(bkk_link)
+    zdb = bkk.join(g_k, on='th-name').dropna()
+    km5cls = km5.fit(zdb.drop(['name','th-name','code','hc-key','geometry'], axis=1).values)
+    labels = list(map(int, km5cls.labels_))
+    resp = make_response(jsonify({'labels': labels, 'keys':list(zdb['hc-key'])}), 200)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
